c04e6f16d840191de2c214596d6dccaec490c3fc
Sameh Abouelsaad <samehvirus@gmail.com>
2020-09-15 10:40:35 +0200

    fixing static files to run on heroku

--- CoffeeTime/settings.py
+++ CoffeeTime/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,10 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 #cors
 CORS_ORIGIN_ALLOW_ALL = False
